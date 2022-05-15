7bcaeb25b40df919d43842f76e3f708ac7283efd
Isaac Ndala <isaacndala111@gmail.com>
2022-05-15 16:55:20 +0100

    Prepare for production

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, '/dist')))
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist'))
+})
+
+app.listen(process.env.PORT || 8080)
