9cdfd93e7240ce8e939d97530a833e03ddd29979
Isaac Ndala <isaacndala111@gmail.com>
2022-05-15 17:11:56 +0100

    Fix server redirection error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, '/dist')))
 app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist'))
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
 app.listen(process.env.PORT || 8080)
