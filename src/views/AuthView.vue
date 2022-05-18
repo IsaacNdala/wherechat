02e0a35e04411314da8c115c4f2db2385c062f9f
Isaac Ndala <isaacndala111@gmail.com>
2022-05-18 17:44:10 +0100

    Ajust height of preloader on auth component

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -350,7 +350,7 @@ export default {
     top: 0;
     left: 0;
     width: 100%;
-    height: 100vh;
+    min-height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
